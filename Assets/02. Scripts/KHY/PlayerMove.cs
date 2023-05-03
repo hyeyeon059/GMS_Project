5416daa899556a1b44854dd1148e4a5f3f3e87cb
hyeyeon059 <leehuijuan0912@gmail.com>
2023-05-03 20:08:10 +0900

    플레이어 콜라이더 수정

--- Assets/02. Scripts/KHY/PlayerMove.cs
+++ Assets/02. Scripts/KHY/PlayerMove.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private float moveSpeed = 5f;
+
     private Rigidbody2D rb;
+    private GameObject flash;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        flash = transform.GetChild(0).gameObject;
     }
 
     void Update()
@@ -23,6 +26,9 @@ public class PlayerMove : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
 
         Vector2 dir = new Vector2(h, v);
-        rb.velocity = dir * moveSpeed;
+        rb.velocity = dir* moveSpeed;
+
+        if (h != 0 || v != 0)
+            flash.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -Mathf.Atan2(h, v) * Mathf.Rad2Deg));
     }
 }

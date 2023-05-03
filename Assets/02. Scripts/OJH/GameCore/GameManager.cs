13ea87e3d08b9eaf26aacc341bb26e3c72f16fe8
phjh <adsf8800@naver.com>
2023-05-03 18:20:19 +0900

    skn
    d<-<

--- Assets/02. Scripts/OJH/GameCore/GameManager.cs
+++ Assets/02. Scripts/OJH/GameCore/GameManager.cs
@@ -5,14 +5,26 @@ using TMPro.EditorUtilities;
 using Unity.VisualScripting;
 using UnityEngine;
 
+enum Item
+{
+    Flash=1,
+};
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public int NowFloor { get; set; }
-    public List<GameObject> FloorPos = new List<GameObject>();
 
-    public bool RoomChanging = false;
-    public bool CanPlayerMove = true;
+    [Header("Empty Object를 만들고 각층끼리 이어지는 지점에 놓는다")]
+    public List<GameObject> FloorPos = new List<GameObject>();
+    public List<int> inventoryItem = new List<int>();
+
+    public bool RoomChanging = false;   //방이동때 켜주는 변수
+    public bool bPlayerMove = true;     //플레이어가 움직일수있는지 체크해주는 변수
+
+    public GameObject Player;           //플레이어 관련 
+
+
     public void Delay(float delay,Action action) => StartCoroutine(SetDelay(delay, action));
 
 
@@ -25,7 +37,7 @@ public class GameManager : MonoBehaviour
 
     public void FloorChange()
     {
-        
+
     }
 
     public IEnumerator SetDelay(float delay,Action action)

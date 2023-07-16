b8decf95b73dd341a09371c7b86bb844849e0489
yoonjunhee <123710778+yoonjunhee@users.noreply.github.com>
2023-07-16 20:04:30 +0900

    ending대사

--- Assets/02. Scripts/YJH/Text.cs
+++ Assets/02. Scripts/YJH/Text.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Text : MonoBehaviour
 {
-    string[] text = {"이 빌어먹을 곳에서 드디어 탈출이다", "(주머니에서 usb를 꺼낸다)",
-        " 이 usb로 신고 해야지", " 평생 콩밥이나 먹으라지. 그 썩을 놈들","그러고 보니 벌써 해가..." ,
-        " 지금이 몇시지?", "아... 출근 할 시간이네", "젠장. 결국 쉬지도 못했잖아.", "그 ㅅㄲ들 싹 고소해버려야지"};
+    string[] text = {"다음 뉴스입니다.", "마약을 이용해 사람들을 속여 금품을 갈취한 사이비종교가 경찰에 체포됬습니다",
+        " 숲 속 별장에 감금되었던 주xx씨의 탈출로 그 실체가 들어났는데요", "교주를 비롯한 간부들은 약을 이용해 집단 환각을 일으켜","신도들에게 기행을 저지른 사실이 밝혀 졌습니다" ,
+        " 생존자인 주xx씨는 현재 병원에서 치료를 받고 있으며", "생명에는 지장이 없는 것으로 밝혀졌습니다", "다음 뉴스 입니다...."};
     void Start()
     {
-        TextManager.Instance.PopText("주인공",text);
+        TextManager.Instance.PopText("뉴스",text);
     }
 
     // Update is called once per frame
